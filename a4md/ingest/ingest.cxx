13d41a771aee5231db009c4d0e19260aa866baea
Stephen <stephenthomas2018@gmail.com>
2018-10-29 13:18:43 +0000

    Making the CMake based project work with dataspaces

--- a4md/ingest/ingest.cxx
+++ a4md/ingest/ingest.cxx
@@ -5,7 +5,13 @@ Ingest::Ingest()
     int nprocs;
     MPI_Comm gcomm;
     MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
-    //dspaces_init(nprocs,1,&gcomm, NULL);
+    gcomm = MPI_COMM_WORLD;
+    int d_result = dspaces_init(nprocs,1,&gcomm, NULL);
+    printf("In Ingest constructor: d_result %d\n",d_result);
+    if (d_result != 0){
+        if (d_result == -12)
+            printf("Error initializing dataspaces. Could not find config file\n");
+    }
 }
 
 Ingest::~Ingest()
--- a4md/ingester.cxx
+++ a4md/ingester.cxx
@@ -1,9 +1,12 @@
 #include "ingest.h"
+#include "mpi.h"
 
 int main (int argc, const char** argv)
 {
+  MPI_Init(NULL,NULL);
   Ingest ingest;
   ingest.run();
 
+  MPI_Finalize();
   return 0;
 }
