e185ad81fc6948e509f50d8b31170374f8e1ed4c
Stephen <stephenthomas2018@gmail.com>
2018-11-08 14:57:43 +0000

    Multiplying box edge length and positions by 10 to compensate for mdtraj dividing by 10

--- examples/sequential_in_transit_vary_N/calc_voronoi_for_frame.py
+++ examples/sequential_in_transit_vary_N/calc_voronoi_for_frame.py
@@ -15,10 +15,19 @@ def analyze(types, points, box_points, step):
         		box_points[5])
     voro = freud.voronoi.Voronoi(box, np.max(box.L)/2) 
     cells = voro.compute(box=box, positions=points).polytopes
-    #print("In analyze: step: ",step)
     if step>=20000:
+        print("In analyze: step: ",step)
+        print('last frame position[0]',points[0])
+        print('freud box', box)
+
         voro.computeVolumes()
-        frq,edges = np.histogram(voro.volumes,range=[0,0.5],bins=30)
+        data = voro.volumes
+        av = np.mean(data)
+        ma = np.max(data)
+        mi = np.min(data)
+        print('mean {} min {} max {}'.format(av,mi,ma))
+        np.savetxt('voro_volumes.txt',data)
+        frq,edges = np.histogram(data,range=[0,50],bins=30)
         np.savetxt('voro_freq.txt',frq)
         np.savetxt('voro_edges.txt',edges)
         print('Number of voronoi cells',len(cells))
