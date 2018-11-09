0eccdc3e421733e2f2e822e16061fffc9fdc9109
Stephen <stephenthomas2018@gmail.com>
2018-11-09 23:39:17 +0000

    Fixing status

--- project/a4md.py
+++ project/a4md.py
@@ -18,7 +18,14 @@ def initialized(job):
     return job.isfile('in.lj')
 
 def simulated(job):
-    return job.isfile('output.dcd')
+    if job.sp.job_type == 'traditional':
+        return job.isfile('output.dcd')
+    else:
+        return analyzed(job)
+
+def analyzed(job):
+    #print('in job',job,job.isfile('voro_freq.txt'))
+    return job.isfile('voro_freq.txt')
 
 @A4MDProject.operation
 @A4MDProject.post(initialized)
@@ -42,38 +49,9 @@ def initialize(job):
 def simulate(job):
     return "cd {job.ws} && mpirun -n {job.sp.NPROCS} lmp_mpi -i in.lj"
 
-def analyze(types, points, box_points, step):
-    #print('-----======= Python : analyze ========-------')
-    print('box points',box_points);
-    box = freud.box.Box(box_points[0],
-        		box_points[1],
-        		box_points[2],
-        		box_points[3],
-        		box_points[4],
-        		box_points[5])
-    voro = freud.voronoi.Voronoi(box, np.max(box.L)/2) 
-    cells = voro.compute(box=box, positions=points).polytopes
-    if step>=20000:
-        print("In analyze: step: ",step)
-        print('last frame position[0]',points[0])
-        print('freud box', box)
-
-        voro.computeVolumes()
-        data = voro.volumes
-        av = np.mean(data)
-        ma = np.max(data)
-        mi = np.min(data)
-        print('mean {} min {} max {}'.format(av,mi,ma))
-        np.savetxt('voro_volumes.txt',data)
-        frq,edges = np.histogram(data,range=[0,50],bins=30)
-        np.savetxt('voro_freq.txt',frq)
-        np.savetxt('voro_edges.txt',edges)
-        print('Number of voronoi cells',len(cells))
-    return 0
-
 @A4MDProject.operation
 @A4MDProject.pre(simulated)
-@A4MDProject.post.isfile('voro_freq.txt')
+@A4MDProject.post(analyzed)
 def analyze_job(job):
     if job.sp.job_type == 'traditional':
         with job:
