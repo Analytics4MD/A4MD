4ad92099b44da49a3a778eaa6c98f283105b6d71
Stephen <stephenthomas2018@gmail.com>
2019-01-15 07:47:13 +0000

    Catching exception in python code and printing it

--- project/analysis_codes/calc_dist_matrix_for_frame.py
+++ project/analysis_codes/calc_dist_matrix_for_frame.py
@@ -12,58 +12,58 @@ from mdtraj.formats import PDBTrajectoryFile
 from mdtraj import Trajectory
 import matplotlib.pyplot as plt
 
-#
-#def pbc_min_image(p1, p2, axes):
-#    dr = p1 - p2
-#    for i, p in enumerate(dr):
-#        if abs(dr[i]) > axes[i]*0.5:
-#            p2[i] = p2[i] + np.sign(dr[i])*axes[i] # Use dr to decide if we need to add or subtract axis
-#    return p2
-#
-#def pbc_traslate(points, axes):
-#    '''Translates a group of points to the minimum image of first point in list
-#    points: list of poits
-#    axes: array, Lx, Ly, Lz
-#    We assume a tetragonal unit cell
-#    By default, all points will be translated into the minimum image
-#    of the first point, but any point can be used
-#    '''
-#    ref_point=points[0]
-#    # Add our min vector to our point
-#    min_image_cords = [pbc_min_image(ref_point, point, axes) for point in points[1:]] # Skip over the first point since its our ref point
-#    min_image_cords.insert(0, ref_point) # Don't forget to add the ref point into the list of points
-#    return np.asarray(min_image_cords)
-#
-#def com(points, masses):
-#    #print(masses,points)
-#    weighted = masses[:,None]*points
-#    M = np.sum(masses)
-#    return np.sum(weighted,axis=0)/M#, M
-#
-#def get_atoms_groups(topology, group_method='residue'):
-#    if group_method == 'residue':
-#        atom_indices = [[a.index for a in r.atoms] for r in topology.residues if r.is_protein]
-#    return np.asarray(atom_indices)
-#
-#def get_masses(topology, group_method=None):
-#    if group_method == 'residue':
-#        masses = [np.asarray([a.element.mass for a in r.atoms]) for r in topology.residues if r.is_protein]
-#    elif group_method == None:
-#        masses = [a.element.mass for a in topology.atoms]
-#    else:
-#        raise NotImplementedError('get_masses not implemented')
-#    return np.asarray(masses)
-#
-#def get_distances(atom_groups, xyzs, use_COM=True, masses=None):
-#    if use_COM and masses is not None:   
-#        coms_by_resid = [com(xyzs[r],masses[r]) for r in atom_groups]
-#        coords = np.asarray(coms_by_resid)
-#        #print(coords)
-#        distances = distance.cdist(coords, coords, 'euclidean')
-#        #distances = coms_by_resid
-#    else:
-#        raise ValueError('use_COM=False is not implemented')
-#    return distances
+
+def pbc_min_image(p1, p2, axes):
+    dr = p1 - p2
+    for i, p in enumerate(dr):
+        if abs(dr[i]) > axes[i]*0.5:
+            p2[i] = p2[i] + np.sign(dr[i])*axes[i] # Use dr to decide if we need to add or subtract axis
+    return p2
+
+def pbc_traslate(points, axes):
+    '''Translates a group of points to the minimum image of first point in list
+    points: list of poits
+    axes: array, Lx, Ly, Lz
+    We assume a tetragonal unit cell
+    By default, all points will be translated into the minimum image
+    of the first point, but any point can be used
+    '''
+    ref_point=points[0]
+    # Add our min vector to our point
+    min_image_cords = [pbc_min_image(ref_point, point, axes) for point in points[1:]] # Skip over the first point since its our ref point
+    min_image_cords.insert(0, ref_point) # Don't forget to add the ref point into the list of points
+    return np.asarray(min_image_cords)
+
+def com(points, masses):
+    #print(masses,points)
+    weighted = masses[:,None]*points
+    M = np.sum(masses)
+    return np.sum(weighted,axis=0)/M#, M
+
+def get_atoms_groups(topology, group_method='residue'):
+    if group_method == 'residue':
+        atom_indices = [[a.index for a in r.atoms] for r in topology.residues if r.is_protein]
+    return np.asarray(atom_indices)
+
+def get_masses(topology, group_method=None):
+    if group_method == 'residue':
+        masses = [np.asarray([a.element.mass for a in r.atoms]) for r in topology.residues if r.is_protein]
+    elif group_method == None:
+        masses = [a.element.mass for a in topology.atoms]
+    else:
+        raise NotImplementedError('get_masses not implemented')
+    return np.asarray(masses)
+
+def get_distances(atom_groups, xyzs, use_COM=True, masses=None):
+    if use_COM and masses is not None:   
+        coms_by_resid = [com(xyzs[r],masses[r]) for r in atom_groups]
+        coords = np.asarray(coms_by_resid)
+        #print(coords)
+        distances = distance.cdist(coords, coords, 'euclidean')
+        #distances = coms_by_resid
+    else:
+        raise ValueError('use_COM=False is not implemented')
+    return distances
 
 
 an_times = []
@@ -74,12 +74,12 @@ with open('signac_statepoint.json', 'r') as f:
     sp = json.load(f)
     ddi = sp['stride']
     nsteps = sp['simulation_time']
-#top = PDBTrajectoryFile(filename='reference.pdb').topology
-#atom_indices_by_resid = get_atoms_groups(top)
-#masses = get_masses(top)
-#plt.figure()
-#fig, ax = plt.subplots()
-#ims = []
+top = PDBTrajectoryFile(filename='reference.pdb').topology
+atom_indices_by_resid = get_atoms_groups(top)
+masses = get_masses(top)
+plt.figure()
+fig, ax = plt.subplots()
+ims = []
 
 def analyze(types, xpoints,ypoints,zpoints, box_points, step):
     print('-----======= Python : analyze ({})========-------'.format(step))
@@ -87,13 +87,19 @@ def analyze(types, xpoints,ypoints,zpoints, box_points, step):
     #print("");
     t=timer() 
     # ---------============= analysis code goes here (start) ===========------------------- 
-    #points = np.vstack((xpoints,ypoints,zpoints)).T
-    #t = Trajectory(points, top, time=step, unitcell_lengths = box_points[:2])
-    #min_img_cords = pbc_traslate(t.xyz[i],t.unitcell_lengths[i])
-    #dist_matrix = get_distances(atom_indices_by_resid,min_img_cords,masses=masses)
-    #im = ax.imshow(dist_matrix, interpolation='nearest')
-    #ims.append([im])
-    #plt.draw()
+    try:
+        points = np.vstack((xpoints,ypoints,zpoints)).T
+        t = Trajectory(points, top, time=step, unitcell_lengths = np.asarray(box_points[:3]))
+        min_img_cords = pbc_traslate(t.xyz[i],t.unitcell_lengths[i])
+        dist_matrix = get_distances(atom_indices_by_resid,min_img_cords,masses=masses)
+        im = ax.imshow(dist_matrix, interpolation='nearest')
+        ims.append([im])
+        plt.draw()
+    except Exception as e:
+        print('!!!!!!!!! Exception caught in python analysis code!!!!!!!!')
+        print(e)
+        raise
+
     # ---------============= analysis code goes here (end)   ===========------------------- 
     an_time = timer()-t
     an_times.append(an_time)
@@ -105,8 +111,8 @@ def analyze(types, xpoints,ypoints,zpoints, box_points, step):
     an_write_time = timer()-t
     an_write_times.append(an_write_time)    
     if step>=nsteps:
-        #ani = manimation.ArtistAnimation(fig, ims, interval=50, blit=True, repeat_delay=1000)
-        #ani.save('dis.gif',writer='imagemagick')
+        ani = manimation.ArtistAnimation(fig, ims, interval=50, blit=True, repeat_delay=1000)
+        ani.save('dis.gif',writer='imagemagick')
         print('------============ reached end of analysis ({}) ==========------------'.format(step))
         with open('signac_job_document.json', 'r') as f:
             job_document = json.load(f)
