ac3034c2e4c97d2d533489a5bf121b49d4ea08ae
Tu Mai Anh Do <tudo@usc.edu>
2019-02-07 17:20:31 -0800

    Add Python path to PyRunner

--- a4md/common/py_runner.h
+++ a4md/common/py_runner.h
@@ -8,14 +8,15 @@
 class PyRunner
 {
     private:
-        int initialize_python();
+        int initialize_python(char* py_path);
         PyObject* m_py_module;
         PyObject* m_py_func;
         const char* m_module_name;
         const char* m_function_name;
     public:
         PyRunner(char* module_name,
-                          char* function_name);
+				 char* function_name,
+				 char* py_path = "");
         ~PyRunner();
         
         int analyze_frame(std::vector<int> types,
