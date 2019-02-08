475c69159189d885e5a278b8d968460d20566ad0
Tu Mai Anh Do <tudo@usc.edu>
2019-02-07 18:25:50 -0800

    Add PDBChunker class and tests on such class

---
+++ a4md/ingest/pdb_chunker.cxx
@@ -0,0 +1,31 @@
+#include "pdb_chunker.h"
+
+PDBChunker::PDBChunker(PyRunner & py_runner,
+					   char* file_path,
+					   int position)
+: m_py_runner(py_runner),
+	m_file_path(file_path),
+	m_position(position)
+{
+	printf("---===== Created PDBChunker with file_path = %s and position = %d \n", m_file_path, m_position);
+}
+
+int PDBChunker::get_position() 
+{
+	return m_position;
+}
+
+int PDBChunker::extract_chunk()
+{	
+	Chunk* chunk;
+	int result	= m_py_runner.extract_frame(m_file_path, m_position, chunk);
+	if (result == 0)
+	{
+		append_chunk(chunk);
+	} 
+	else 	
+	{
+		fprintf(stderr, "PDBChunker::extract_chunk is not able to extract chunk\n");
+	}
+	return result;
+}
---
+++ a4md/ingest/pdb_chunker.h
@@ -0,0 +1,19 @@
+#ifndef __PDB_CHUNKER_H__
+#define __PDB_CHUNKER_H__
+#include "chunker.h"
+#include "py_runner.h"
+
+class PDBChunker : public Chunker
+{
+	private:
+		PyRunner & m_py_runner;
+		char* m_file_path;
+		int m_position; 	
+		
+	public:
+		PDBChunker(PyRunner & py_runner, char* file_path, int position = 0);
+		int extract_chunk();
+		int get_position();
+};
+
+#endif
