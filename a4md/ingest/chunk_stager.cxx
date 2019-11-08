387250b95df0cce8933bd135d06023b071c30899
Tu Mai Anh Do <tudo@usc.edu>
2019-11-08 15:30:16 -0800

    Add TAU timers to chunk_stagers

--- a4md/ingest/chunk_stager.cxx
+++ a4md/ingest/chunk_stager.cxx
@@ -1,5 +1,8 @@
 #include "chunk_stager.h"
 #include <vector>
+#ifdef TAU_PERF
+#include <TAU.h>
+#endif
 
 ChunkStager::ChunkStager(ChunkReader & chunk_reader, ChunkWriter & chunk_writer)
 : m_chunk_reader(chunk_reader),
@@ -26,10 +29,25 @@ bool ChunkStager::stage_chunks(int num_chunks)
 void ChunkStager::stage_chunks(unsigned long int chunk_id_from, unsigned long int chunk_id_to)
 {
     printf("ChunkStager::stage_chunks() --> Stage chunks from %lu to %lu\n", chunk_id_from, chunk_id_to);
+#ifdef TAU_PERF
+    TAU_STATIC_TIMER_START("total_extract_chunks_time");
+    TAU_DYNAMIC_TIMER_START("step_extract_chunks_time");
+#endif
     std::vector<Chunk*> chunks = m_chunk_reader.read_chunks(chunk_id_from, chunk_id_to);
+#ifdef TAU_PERF
+    TAU_DYNAMIC_TIMER_STOP("step_extract_chunks_time");
+    TAU_STATIC_TIMER_STOP("total_extract_chunks_time");
+
+    TAU_STATIC_TIMER_START("total_write_chunks_time");
+    TAU_DYNAMIC_TIMER_START("step_write_chunks_time");
+#endif
     m_chunk_writer.write_chunks(chunks);
     for (Chunk* chunk : chunks)
     {
         free_chunk(chunk);
     }
+#ifdef TAU_PERF
+    TAU_DYNAMIC_TIMER_STOP("step_write_chunks_time");
+    TAU_STATIC_TIMER_STOP("total_write_chunks_time");
+#endif
 }
