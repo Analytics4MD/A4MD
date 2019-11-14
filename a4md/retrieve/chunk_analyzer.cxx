b42ba02869f540ae2b87cadd13596d965007247e
Tu Mai Anh Do <tudo@usc.edu>
2019-11-14 15:17:26 -0500

    Fix TAU timer name

--- a4md/retrieve/chunk_analyzer.cxx
+++ a4md/retrieve/chunk_analyzer.cxx
@@ -25,8 +25,8 @@ void ChunkAnalyzer::analyze_chunks(int chunk_id_from, int chunk_id_to)
 #endif
     auto chunks = m_chunk_reader.read_chunks(chunk_id_from, chunk_id_to);
 #ifdef TAU_PERF
-    TAU_DYNAMIC_TIMER_STOP("step_read_chunks");
-    TAU_STATIC_TIMER_STOP("total_read_chunks");
+    TAU_DYNAMIC_TIMER_STOP("step_read_chunks_time");
+    TAU_STATIC_TIMER_STOP("total_read_chunks_time");
     //TAU_TRACK_MEMORY_FOOTPRINT();
     //TAU_TRACK_MEMORY_FOOTPRINT_HERE();
 
