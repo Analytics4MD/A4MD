7c006dd0dbf5385b08159e93f152abd89055648a
Tu Mai Anh Do <tudo@usc.edu>
2020-05-31 22:54:47 -0700

    Update Python scripts in test cases

--- a4md/common/test/test_direct.py
+++ a4md/common/test/test_direct.py
@@ -1,5 +1,5 @@
 import numpy as np
 
 def direct(types, posx, posy, posz, box, step):
-    return [list(types), list(posx), list(posy), list(posz), list(box), step, 0]
+    return types, posx, posy, posz, box, step
 
