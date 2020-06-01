75f2126bbbecc6d1fb54901d60079337826f5ffb
Tu Mai Anh Do <tudo@usc.edu>
2020-05-31 22:49:13 -0700

    New MDStager constructor with ChunkOperator

--- a4md/common/md_stager.h
+++ a4md/common/md_stager.h
@@ -1,15 +1,16 @@
 #ifndef __MD_STAGER_H__
 #define __MD_STAGER_H_
 #include "chunk_stager.h"
-#include "chunk_writer.h"
-#include "chunk_reader.h"
 
 class MDStager : public ChunkStager
 {
     public:
-        MDStager(ChunkReader & chunk_reader, ChunkWriter & chunk_writer);
+        MDStager(ChunkReader* chunk_reader, ChunkWriter* chunk_writer);
+        MDStager(ChunkReader* chunk_reader, ChunkOperator* chunk_operator, ChunkWriter* chunk_writer);
         ~MDStager();
+
         void free_chunk(Chunk* chunk) override;
+        void free_chunks(std::vector<Chunk*> chunks) override;
 };
 
 #endif
