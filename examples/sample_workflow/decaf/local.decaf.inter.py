d9c7426a5ffa81ed272742eaf1beea780fc14d19
Tu Mai Anh Do <tudo@usc.edu>
2020-06-08 18:12:12 -0700

    Refactor example local scripts of Decaf

--- examples/sample_workflow/decaf/local.decaf.inter.py
+++ examples/sample_workflow/decaf/local.decaf.inter.py
@@ -22,20 +22,21 @@ wf = imp.load_source('workflow', os.environ['DECAF_PREFIX'] + '/python/decaf.py'
 #  dataflow can be overlapped, but currently all disjoint procs (simplest case)
 
 # --- Graph definition ---
-
+job_name = "local.decaf.inter"
 nproducers = int(sys.argv[1])
 nintermediators_per_producer = int(sys.argv[2])
 nconsumers_per_intermediators = int(sys.argv[3])
 
+
 process_id = -1;
 for prod_id in range(nproducers):
 	process_id += 1
-	prod = wf.Node("producer"+str(prod_id), start_proc=process_id, nprocs=1, func='', cmdline='./producer decaf inter.json ./load.py extract_frame 5 200 0')
+	prod = wf.Node("producer"+str(prod_id), start_proc=process_id, nprocs=1, func='', cmdline='./producer decaf ' + job_name + '.json ./load.py extract_frame 5 200 0')
 	prod.addOutputPort("out")
 
 	for inter_id in range(nintermediators_per_producer):
 		process_id += 1
-		inter = wf.Node("intermediator"+str(prod_id)+"."+str(inter_id), start_proc=process_id, nprocs=1, func='', cmdline='./intermediator decaf inter.json ./forward.py direct 5')
+		inter = wf.Node("intermediator"+str(prod_id)+"."+str(inter_id), start_proc=process_id, nprocs=1, func='', cmdline='./intermediator decaf ' + job_name + '.json ./forward.py direct 5')
 		inter.addInputPort("in")
 		inter.addOutputPort("out")
 
@@ -43,11 +44,11 @@ for prod_id in range(nproducers):
 
 		for con_id in range(nconsumers_per_intermediators):
 			process_id += 1
-			con = wf.Node("consumer"+str(prod_id)+"."+str(inter_id)+"."+str(con_id), start_proc=process_id, nprocs=1, func='', cmdline='./consumer decaf inter.json ./compute.py analyze 5')
+			con = wf.Node("consumer"+str(prod_id)+"."+str(inter_id)+"."+str(con_id), start_proc=process_id, nprocs=1, func='', cmdline='./consumer decaf ' + job_name + '.json ./compute.py analyze 5')
 			con.addInputPort("in")
 
 			inter_con = wf.Edge(inter.getOutputPort("out"), con.getInputPort("in"), start_proc=0, nprocs=0, func='', prod_dflow_redist='count', dflow_con_redist='count')
 
 # --- convert the nx graph into a workflow data structure and run the workflow ---
 
-wf.processGraph("inter")
+wf.processGraph(job_name)
--- examples/sample_workflow/decaf/local.decaf.prod_con.py
+++ examples/sample_workflow/decaf/local.decaf.prod_con.py
@@ -24,20 +24,21 @@ wf = imp.load_source('workflow', os.environ['DECAF_PREFIX'] + '/python/decaf.py'
 # --- Graph definition ---
 nproducers = int(sys.argv[1])
 nconsumers_per_producer = int(sys.argv[2])
+job_name = "local.decaf.prod_con"
 
 process_id = -1
 for prod_id in range(nproducers):
 	process_id += 1
-	prod = wf.Node("producer"+str(prod_id), start_proc=process_id, nprocs=1, func='', cmdline='./producer decaf prod_con.json ./load.py extract_frame 5 200 0')
+	prod = wf.Node("producer"+str(prod_id), start_proc=process_id, nprocs=1, func='', cmdline='./producer decaf ' + job_name + '.json ./load.py extract_frame 5 200 0')
 	outPort = prod.addOutputPort("out")
 
 	for con_id in range(nconsumers_per_producer):
 		process_id += 1
-		con = wf.Node("consumer"+str(prod_id)+"."+str(con_id), start_proc=process_id, nprocs=1, func='', cmdline='./consumer decaf prod_con.json ./compute.py analyze 5')
+		con = wf.Node("consumer"+str(prod_id)+"."+str(con_id), start_proc=process_id, nprocs=1, func='', cmdline='./consumer decaf ' + job_name + '.json ./compute.py analyze 5')
 		inPort = con.addInputPort("in")
 
 		prod_con = wf.Edge(prod.getOutputPort("out"), con.getInputPort("in"), start_proc=0, nprocs=0, func='', prod_dflow_redist='count', dflow_con_redist='count')
 
 # --- convert the nx graph into a workflow data structure and run the workflow ---
 
-wf.processGraph("prod_con")
+wf.processGraph(job_name)
