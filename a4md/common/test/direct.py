c61ab0cc07545a2a4d40338c564b574c54b90399
Tu Mai Anh Do <tudo@usc.edu>
2020-09-11 15:13:36 -0700

    Add Runner tests

--- a4md/common/test/direct.py
+++ a4md/common/test/direct.py
@@ -4,4 +4,4 @@ def mdchunk_to_mdchunk(types, posx, posy, posz, box, step):
     return types, posx, posy, posz, box, step
 
 def mdchunk_to_cvchunk(types, posx, posy, posz, box, step):
-    return posx, posy
+    return posx
