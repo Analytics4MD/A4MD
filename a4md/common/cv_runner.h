5a3c0f8c613308b9da3d9b8d729f4a38f3401488
Tu Mai Anh Do <tudo@usc.edu>
2020-09-11 12:12:42 -0700

    Add CVRunner

---
+++ a4md/common/cv_runner.h
@@ -0,0 +1,18 @@
+#ifndef __CV_RUNNER_H__
+#define __CV_RUNNER_H__
+#include "py_runner.h"
+#include "cv_chunk.h"
+
+class CVRunner : public PyRunner 
+{
+	private:
+	public:
+		CVRunner(char* module_name, char* function_name, char* py_path = (char*)"");
+		~CVRunner();
+
+		void input_chunk(Chunk* chunk) override;
+        Chunk* output_chunk(unsigned long int chunk_id) override;
+        Chunk* direct_chunk(Chunk* chunk) override;
+};
+
+#endif /* __CV_RUNNER_H__ */
