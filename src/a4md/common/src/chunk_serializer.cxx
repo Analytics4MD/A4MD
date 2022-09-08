db9269ec545548ff96de09c73c5f55acd8c2165f
Jack Marquez <jmarque4@tellico-master0.local>
2022-09-08 12:14:24 -0400

    A4-24: created the binaries

--- src/a4md/common/src/chunk_serializer.cxx
+++ src/a4md/common/src/chunk_serializer.cxx
@@ -8,44 +8,43 @@
 #include <boost/archive/binary_iarchive.hpp>
 
 
-
 template<typename SerializableContainer>
 ChunkSerializer<SerializableContainer>::ChunkSerializer()
 {
-	printf("---===== Initialized ChunkSerializer on %s\n", typeid(SerializableContainer).name());
+    printf("---===== Initialized ChunkSerializer on %s\n", typeid(SerializableContainer).name());
 }
 
 template<typename SerializableContainer>
 ChunkSerializer<SerializableContainer>::~ChunkSerializer()
 {
-	printf("---===== Finalized ChunkSerializer on %s\n", typeid(SerializableContainer).name());
+    printf("---===== Finalized ChunkSerializer on %s\n", typeid(SerializableContainer).name());
 }
 
 template<typename SerializableContainer>
 bool ChunkSerializer<SerializableContainer>::serialize(const SerializableContainer& serializable_container, std::string& serialized_buffer)
 {
-	if (!serialized_buffer.empty())
-	{
-		printf("---===== ERROR: ChunkSerializer::serialize() --> Serialized buffer is not empty\n");
-		return false;
-	}
-	boost::iostreams::back_insert_device<std::string> inserter(serialized_buffer);
-	boost::iostreams::stream<boost::iostreams::back_insert_device<std::string> > s(inserter);
-	boost::archive::binary_oarchive oa(s);
-	oa << serializable_container;
-	s.flush();
-	return true;
+    if (!serialized_buffer.empty())
+    {
+        printf("---===== ERROR: ChunkSerializer::serialize() --> Serialized buffer is not empty\n");
+        return false;
+    }
+    boost::iostreams::back_insert_device<std::string> inserter(serialized_buffer);
+    boost::iostreams::stream<boost::iostreams::back_insert_device<std::string> > s(inserter);
+    boost::archive::binary_oarchive oa(s);
+    oa << serializable_container;
+    s.flush();
+    return true;
 }
 
 template<typename SerializableContainer>
 bool ChunkSerializer<SerializableContainer>::deserialize(SerializableContainer& serializable_container, const std::string& serialized_buffer)
 {
-	if (serialized_buffer.empty())
-	{
-		printf("---===== ERROR: ChunkSerializer::deserialize() --> Serialized buffer is empty\n");
-		return false;
-	}
-	boost::iostreams::basic_array_source<char> device(serialized_buffer.data(), serialized_buffer.size());
+    if (serialized_buffer.empty())
+    {
+        printf("---===== ERROR: ChunkSerializer::deserialize() --> Serialized buffer is empty\n");
+        return false;
+    }
+    boost::iostreams::basic_array_source<char> device(serialized_buffer.data(), serialized_buffer.size());
     boost::iostreams::stream<boost::iostreams::basic_array_source<char> > sout(device);
     boost::archive::binary_iarchive ia(sout);
     ia >> serializable_container;
@@ -55,12 +54,12 @@ bool ChunkSerializer<SerializableContainer>::deserialize(SerializableContainer& 
 template<typename SerializableContainer>
 bool ChunkSerializer<SerializableContainer>::deserialize(SerializableContainer& serializable_container, const char* serialized_buffer, const size_t& buffer_size)
 {
-	if (serialized_buffer == nullptr)
-	{
-		printf("---===== ERROR: ChunkSerializer::deserialize() --> Serialized buffer is nullptr\n");
-		return false;
-	}
-	boost::iostreams::basic_array_source<char> device(serialized_buffer, buffer_size);
+    if (serialized_buffer == nullptr)
+    {
+        printf("---===== ERROR: ChunkSerializer::deserialize() --> Serialized buffer is nullptr\n");
+        return false;
+    }
+    boost::iostreams::basic_array_source<char> device(serialized_buffer, buffer_size);
     boost::iostreams::stream<boost::iostreams::basic_array_source<char> > sout(device);
     boost::archive::binary_iarchive ia(sout);
     ia >> serializable_container;
@@ -68,3 +67,4 @@ bool ChunkSerializer<SerializableContainer>::deserialize(SerializableContainer& 
 }
 
 template class ChunkSerializer<SerializableChunk>;
+
