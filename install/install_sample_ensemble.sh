198d233b9fbfc10efd2db530552dde0adf662150
Jack Marquez <jackdmarquez@gmail.com>
2023-02-22 21:00:14 +0000

    testing path for binaries

--- install/install_sample_ensemble.sh
+++ install/install_sample_ensemble.sh
@@ -9,7 +9,7 @@ git clone git@github.com:Analytics4MD/A4MD-sample-workflow.git sampleEnsemble
 cd sampleEnsemble
 mkdir build
 cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=.. -DA4MD_PREFIX=${install_dir}/../a4md-test -DDATASPACES_PREFIX=${app_install_dir}/dataspaces
+cmake .. -DCMAKE_INSTALL_PREFIX=.. -DA4MD_PREFIX=$HOME/a4md-test -DDATASPACES_PREFIX=${app_install_dir}/dataspaces
 make
 make install
 cd ../bin/
