b600b99f94ae3c42b6472201d2c82f828591e36e
Jack Marquez <jackdmarquez@gmail.com>
2023-09-06 18:07:32 +0000

    fix(Folders)::modifying folder structure according Code Ocean folders

--- install/install_a4md.sh
+++ install/install_a4md.sh
@@ -6,7 +6,7 @@ app_install_dir=$2
 cd ${install_dir}
 mkdir build
 cd ${install_dir}/build
-cmake .. -DCMAKE_INSTALL_PREFIX=$HOME/a4md-test -DDATASPACES_PREFIX=${app_install_dir}/dataspaces
+cmake .. -DCMAKE_INSTALL_PREFIX=${install_dir}/a4md-test -DDATASPACES_PREFIX=${app_install_dir}/dataspaces
 make
 make install
 cd ${install_dir}
--- install/install_sample_ensemble.sh
+++ install/install_sample_ensemble.sh
@@ -4,12 +4,12 @@ install_dir=$1
 app_install_dir=$2
 
 echo 'Cloning and building sample workflow' 
-cd
+cd ${install_dir}
 git clone git@github.com:Analytics4MD/A4MD-sample-workflow.git sampleEnsemble
 cd sampleEnsemble
 mkdir build
 cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=.. -DA4MD_PREFIX=$HOME/a4md-test -DDATASPACES_PREFIX=${app_install_dir}/dataspaces
+cmake .. -DCMAKE_INSTALL_PREFIX=.. -DA4MD_PREFIX=${install_dir}/a4md-test -DDATASPACES_PREFIX=${app_install_dir}/dataspaces
 make
 make install
 cd ../bin/
